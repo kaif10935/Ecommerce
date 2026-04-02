abb352f44a1bb03d7896c2df1168dbafac1542a5
kaif <kaif10935@gmail.com>
2026-04-02 19:34:21 +0530

    changesMadeInCategoryController

--- src/main/java/com/springboot/Ecommerce/controller/CategoryController.java
+++ src/main/java/com/springboot/Ecommerce/controller/CategoryController.java
@@ -5,13 +5,9 @@ import com.springboot.Ecommerce.exceptions.ResourceNotFoundException;
 import com.springboot.Ecommerce.model.Category;
 import com.springboot.Ecommerce.service.CategoryService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
